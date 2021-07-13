7d4334879e4fafab713e3c4b2c84d6a9f59a2bd9
carlosbult <acevedo.bult@gmail.com>
2021-07-13 16:29:00 -0400

    Fix2

--- src/figures.js
+++ src/figures.js
@@ -1,72 +1,92 @@
 // Codigo de calculo de Cuadrado
 
 function calculoAreaCuadro() {
-    const inputCuadrado = document.getElementById("inputCuadrado");
-    const ladoCuadrado = inputCuadrado.value;
-    const areaCuadrado = ladoCuadrado ** 2;
+  const inputCuadrado = document.getElementById("inputCuadrado");
+  const ladoCuadrado = inputCuadrado.value;
+  const areaCuadrado = ladoCuadrado ** 2;
 
-    const resultadoCuadrado = document.getElementById('resultadoArea');
+  const resultadoCuadrado = document.getElementById("resultadoArea");
+
+  if (ladoCuadrado === "") {
+    resultadoCuadrado.textContent = `Por favor ingresa una medida`;
+  } else {
     resultadoCuadrado.textContent = `El area del cuadrado es de: ${areaCuadrado} cm2`;
-};
+  }
+}
 
 function calculoPerimetroCuadro() {
-    const inputCuadrado = document.getElementById("inputCuadrado");
-    const ladoCuadrado = inputCuadrado.value;
-    const perimetroCuadrado = ladoCuadrado * 4;
+  const inputCuadrado = document.getElementById("inputCuadrado");
+  const ladoCuadrado = inputCuadrado.value;
+  const perimetroCuadrado = ladoCuadrado * 4;
 
-    const resultadoCuadrado = document.getElementById('resultadoArea');
+  const resultadoCuadrado = document.getElementById("resultadoArea");
+
+  if (ladoCuadrado === "") {
+    resultadoCuadrado.textContent = `Por favor ingresa una medida`;
+  } else {
     resultadoCuadrado.textContent = `El Perimetro del cuadrado es de: ${perimetroCuadrado} cm^2`;
-};
+  }
+}
 
 // Codigo de calculo de Triangulo
 
 function calculoAreaTriangulo() {
-    const ladoDerecho = document.getElementById('ladoDerecho');
-    const valueDerecho = ladoDerecho.value;
-    const base = document.getElementById('base');
-    const valueBase = base.value;
+  const ladoDerecho = document.getElementById("ladoDerecho");
+  const valueDerecho = ladoDerecho.value;
+  const base = document.getElementById("base");
+  const valueBase = base.value;
 
-    const alturaTriangulo = Math.sqrt((valueDerecho ** 2) - ((valueBase / 2) ** 2));
-    const areaTriangulo = (valueBase * alturaTriangulo) / 2;
+  const alturaTriangulo = Math.sqrt(valueDerecho ** 2 - (valueBase / 2) ** 2);
+  const areaTriangulo = (valueBase * alturaTriangulo) / 2;
 
-    const resultadoTriangulo = document.getElementById('resultadoAreaTriangulo');
-    resultadoTriangulo.textContent = `El Area del triangulo es de: ${areaTriangulo} cm2`;
-};
+  const resultadoTriangulo = document.getElementById("resultadoAreaTriangulo");
+  resultadoTriangulo.textContent = `El Area del triangulo es de: ${areaTriangulo} cm2`;
+}
 
 function calculoPerimetroTriangulo() {
-    const ladoDerecho = document.getElementById('ladoDerecho');
-    const valueDerecho = ladoDerecho.value;
-    const ladoIzquierdo = document.getElementById('ladoIzquierdo');
-    const valueIzquierdo = ladoIzquierdo.value;
-    const base = document.getElementById('base');
-    const valueBase = base.value;
+  const ladoDerecho = document.getElementById("ladoDerecho");
+  const valueDerecho = ladoDerecho.value;
+  const ladoIzquierdo = document.getElementById("ladoIzquierdo");
+  const valueIzquierdo = ladoIzquierdo.value;
+  const base = document.getElementById("base");
+  const valueBase = base.value;
 
-    const perimetroTriangulo = valueDerecho + valueIzquierdo + valueBase;
+  const perimetroTriangulo = valueDerecho + valueIzquierdo + valueBase;
 
-    const resultadoTriangulo = document.getElementById('resultadoAreaTriangulo');
-    resultadoTriangulo.textContent = `El Perimetro del triangulo es de: ${perimetroTriangulo} cm`;
-};
+  const resultadoTriangulo = document.getElementById("resultadoAreaTriangulo");
+  resultadoTriangulo.textContent = `El Perimetro del triangulo es de: ${perimetroTriangulo} cm`;
+}
 
 // Codigo de calculo de Circulo
 
 function calculoAreaCirculo() {
-    const inputCuadrado = document.getElementById("inputCirculo");
-    const ladoCuadrado = inputCuadrado.value;
-    const pi = Math.PI;
-    const areaCirculo = (ladoCuadrado ** 2) * pi;
+  const inputCuadrado = document.getElementById("inputCirculo");
+  const ladoCuadrado = inputCuadrado.value;
+  const pi = Math.PI;
+  const areaCirculo = ladoCuadrado ** 2 * pi;
 
-    const resultadoCuadrado = document.getElementById('resultadoAreaCirculo');
+  const resultadoCuadrado = document.getElementById("resultadoAreaCirculo");
+
+  if (ladoCuadrado === "") {
+    resultadoCuadrado.textContent = `Por favor ingresa una medida`;
+  } else {
     resultadoCuadrado.textContent = `El Area del circulo es de: ${areaCirculo} cm2`;
-};
+  }
+}
 
 function calculoCircunferenciaCirculo() {
-    const inputCuadrado = document.getElementById("inputCirculo");
-    const ladoCuadrado = inputCuadrado.value;
-    const pi = Math.PI;
-    const diametro = ladoCuadrado * 2;
+  const inputCirculo = document.getElementById("inputCirculo");
+  const ladoCuadrado = inputCirculo.value;
+  const pi = Math.PI;
+  const diametro = ladoCuadrado * 2;
 
-    const circunferencia = diametro * pi;
+  const circunferencia = diametro * pi;
 
-    const resultadoCuadrado = document.getElementById('resultadoAreaCirculo');
+  const resultadoCuadrado = document.getElementById("resultadoAreaCirculo");
+
+  if (ladoCuadrado === "") {
+    resultadoCuadrado.textContent = `Por favor ingresa una medida`;
+  } else {
     resultadoCuadrado.textContent = `La Circunferencia del circulo es de: ${circunferencia} cm`;
-};
+  }
+}
--- src/percentage.js
+++ src/percentage.js
@@ -1,85 +1,119 @@
 function calcularDescuento() {
-    const precio = document.getElementById('precio').value;
-    let descuento = document.getElementById('descuento').value;
+  const precio = document.getElementById("precio").value;
+  let descuento = document.getElementById("descuento").value;
+  const respuesta = document.getElementById("precioFinal");
 
+  if (precio === "" && descuento === "") {
+    respuesta.style = "color: var(--equal-key-bg)";
+    respuesta.textContent = `Por favor coloca un precio y valor de descuento para calcular`;
+  } else if (precio === "") {
+    respuesta.style = "color: var(--equal-key-bg)";
+    respuesta.textContent = `Por favor coloca un precio para calcular el valor del descuento`;
+  } else if (descuento === "") {
+    respuesta.style = "color: var(--equal-key-bg)";
+    respuesta.textContent = `Por favor coloca el valor del descuento para calcular el precio final`;
+  } else {
     const precioFinal = (precio * (100 - descuento)) / 100;
-
-    const respuesta = document.getElementById('precioFinal');
-    respuesta.textContent = `El precio con un descuento de ${descuento}% es de $${precioFinal}`
-
+    respuesta.style = "color: var(--screen-color)";
+    respuesta.textContent = `El precio con un descuento de ${descuento}% es de $${precioFinal}`;
+  }
 }
 
 function utilizarCupon() {
-    const parentElement = document.getElementById('form');
+  const parentElement = document.getElementById("form");
 
-    const labelDescuento = document.getElementById('labelDescuento');
-    labelDescuento.textContent = 'Elige un cupon';
+  const cuponButton = document.getElementById("cuponButton");
+  cuponButton.value = "Colocar Descuento";
+  cuponButton.removeAttribute("onclick");
 
-    const inputDescuento = document.getElementById('descuento');
-    inputDescuento.setAttribute('id', 'cupon');
-    inputDescuento.setAttribute('type', 'text');
+  const labelDescuento = document.getElementById("labelDescuento");
+  labelDescuento.textContent = "Elige un cupon";
 
-    const selectInput = document.createElement('select');
+  const inputDescuento = document.getElementById("descuento");
 
+  const selectInput = document.createElement("select");
+  selectInput.setAttribute("id", "cupon");
+  selectInput.setAttribute("type", "text");
+  selectInput.style =
+    "padding: 0.5em; background-color: var(--screen-bg); margin: 1em; border-radius: 1em; border: 0.1px solid var(--reset-key-bg); color: var(--second-number)";
 
-    parentElement.replaceChild(selectInput, inputDescuento);
+  parentElement.replaceChild(selectInput, inputDescuento);
 
-    const cupones = [
-        {
-            cupon:'DESC%10',
-            descuento: 10
-        },
-        {
-            cupon:'DESC%20',
-            descuento: 20
-        },
-        {
-            cupon:'DESC%30',
-            descuento: 30
-        },
-        {
-            cupon:'DESC%40',
-            descuento: 40
-        },
-        {
-            cupon:'DESC%50',
-            descuento: 50
-        },
-        {
-            cupon:'DESC%60',
-            descuento: 60
-        }
-    ];
+  const coupons = [
+    {
+      cupon: "DESC%10",
+      descuento: 10,
+    },
+    {
+      cupon: "DESC%20",
+      descuento: 20,
+    },
+    {
+      cupon: "DESC%30",
+      descuento: 30,
+    },
+    {
+      cupon: "DESC%40",
+      descuento: 40,
+    },
+    {
+      cupon: "DESC%50",
+      descuento: 50,
+    },
+    {
+      cupon: "DESC%60",
+      descuento: 60,
+    },
+  ];
 
-    cupones.forEach(coupon => {
-        let options = document.createElement('option');
-        selectInput.append(options);
-        options.textContent = `${coupon.cupon}`
-    });
+  coupons.forEach((coupon) => {
+    let options = document.createElement("option");
+    selectInput.append(options);
+    options.textContent = `${coupon.cupon}`;
+  });
 
+  const calculo = document.getElementById("calculoDescuento");
+  calculo.removeAttribute("onclick");
 
-    const calculo = document.getElementById('calculoDescuento');
-    calculo.removeAttribute('onclick');
+  // ==========================================================//
 
+  cuponButton.addEventListener("click", () => {
+    if (cuponButton.value === "Colocar Descuento") {
+      cuponButton.value = "Utilizar Cupon";
+      cuponButton.setAttribute("onclick", "utilizarCupon()");
+      const parentElement = document.getElementById("form");
 
-    calculo.addEventListener('click', () => {
+      const labelDescuento = document.getElementById("labelDescuento");
+      labelDescuento.textContent = "Descuento a calcular";
 
-        const precio = document.getElementById('precio').value;
-        const respuesta = document.getElementById('precioFinal');
-        const cuponValue = selectInput.value;
+      parentElement.replaceChild(inputDescuento, selectInput);
 
-        const isCuponValido = (coupon) => {
-            return coupon.cupon === cuponValue;
-        }
+      const calculo = document.getElementById("calculoDescuento");
+      calculo.setAttribute("onclick", "calcularDescuento()");
+    }
+  });
 
-        const cuponValido = cupones.find(isCuponValido);
+  //============================================================//
 
-        if (!cuponValido){
-            respuesta.textContent = `El cupon ${descuento} no es valido`;
-        } else {
-            descuento = cuponValido.descuento;
-            const precioFinal = (precio * (100 - descuento)) / 100;
-            respuesta.textContent = `El precio con el uso de  un cupon ${cuponValue} es de $${precioFinal}`
-        };
-    })
+  calculo.addEventListener("click", () => {
+    const precio = document.getElementById("precio").value;
+    const respuesta = document.getElementById("precioFinal");
+    const cuponValue = selectInput.value;
+
+    const isCuponValido = (coupon) => {
+      return coupon.cupon === cuponValue;
+    };
+
+    const cuponValido = coupons.find(isCuponValido);
+
+    if (precio === "") {
+      respuesta.style = "color: var(--equal-key-bg)";
+      respuesta.textContent = `Por favor ingresa un precio para uso del cupon`;
+    } else {
+      descuento = cuponValido.descuento;
+      const precioFinal = (precio * (100 - descuento)) / 100;
+      respuesta.style = "color: var(--screen-color)";
+      respuesta.textContent = `El precio con el uso de  un cupon ${cuponValue} es de $${precioFinal}`;
+    }
+  });
 }
